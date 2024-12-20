25c3edde627cf8c724e824b081e970d09e7daf69
Boluwatife Oyekanmi <enitanboluwatife5@gmail.com>
2024-12-20 16:39:12 +0100

    added cnetre regristration

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -57,7 +57,7 @@ export default function Navbar() {
           </Link>
           <Link 
             href="/auth/register/centre"
-            className="bg-blue-600 text-white mt-4 w-full text-center px-4 py-2 rounded-lg hover:bg-blue-700"
+            className="bg-blue-600 text-white mt-4 w-full text-center block px-4 py-2 rounded-lg hover:bg-blue-700"
           >
             Register Centre
           </Link>
